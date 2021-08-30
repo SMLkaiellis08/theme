57f7f56f1d3bbca69ba929fe501d5e7698987b7f
SMLkaiellis08 <65583319+SMLkaiellis08@users.noreply.github.com>
2021-08-30 14:06:43 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,17 +1,19 @@
 -- First.lua
 if other == true then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/opBandwidth/Magma-Core/main/Jailbreak/Main.lua"))();
+    print("'other_gui' Script Enabled")
 elseif other == false then
-  print("'Other' Script Disabled")
+  print("'other_gui' Script Disabled")
 else
-  error("Value 'other' is not set to True Or False")
+  error("Value 'other_gui' is not set to True Or False")
 end
 -- Second.lua
 if rob == true then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/wawsdasdacx/ohascriptnrrewading/main/jbsaxcriptidk1"))();
+    print("'auto_rob' Script Enabled")
 elseif rob == false then
-  print("'Rob' Script Disabled")
+  print("'auto_rob' Script Disabled")
 else
-  error("Value 'Rob' is not set to True Or False")
+  error("Value 'auto_rob' is not set to True Or False")
 end
 -- End

9752ae724b3d23d2b022f86c007b912489683356
SMLkaiellis08 <65583319+SMLkaiellis08@users.noreply.github.com>
2021-08-30 13:37:54 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3,15 +3,9 @@ if other == "true" then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/opBandwidth/Magma-Core/main/Jailbreak/Main.lua"))();
 elseif other == "false" then
   print("'Other' Script Disabled")
-else
-    error("'other' Variable not true/false")
-    print("'other' Variable not true/false")
 -- Second.lua
 if rob == "true" then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/wawsdasdacx/ohascriptnrrewading/main/jbsaxcriptidk1"))();
 elseif rob == "false" then
     print("'Rob' Script Disabled")
-else
-    error("'rob' Variable not true/false")
-    print("'rob' Variable not true/false")
 -- End

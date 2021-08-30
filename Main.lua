599c660debad99643974d335cde7f4d205e12140
SMLkaiellis08 <65583319+SMLkaiellis08@users.noreply.github.com>
2021-08-30 13:59:48 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,13 +1,17 @@
 -- First.lua
-if other == "true" then
+if other == true then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/opBandwidth/Magma-Core/main/Jailbreak/Main.lua"))();
-elseif other == "false" then
+elseif other == false then
   print("'Other' Script Disabled")
- end
+else
+  error("Value 'other' is not set to True Or False")
+end
 -- Second.lua
-if rob == "true" then
+if rob == true then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/wawsdasdacx/ohascriptnrrewading/main/jbsaxcriptidk1"))();
-elseif rob == "false" then
+elseif rob == false then
   print("'Rob' Script Disabled")
+else
+  error("Value 'Rob' is not set to True Or False")
 end
 -- End

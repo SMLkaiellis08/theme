19a590493ea2c00ff5ee693778c8ee27fa097fb7
SMLkaiellis08 <65583319+SMLkaiellis08@users.noreply.github.com>
2021-08-30 15:15:35 +0100

    Update main.lua

--- Scripts/main.lua
+++ Scripts/main.lua
@@ -7,24 +7,58 @@ print("                                  --Gui Code Links--")
 print("AutoRob Script: ", AutoRob_Script)
 print("Other Gui Script: ", other_Script)
 
+-- loaded?
+if auto_rob_loaded == true then
+    -- game.StarterGui:SetCore("SendNotification", {Title="Magma-mc.net"; Text="Auto Rob Script is Enabled"; Duration=5;})
+    print("Auto Rob Script is Enabled")
+end
+
+if other_gui_loaded == true then
+    -- game.StarterGui:SetCore("SendNotification", {Title="Magma-mc.net"; Text="Other Gui Script is Enabled"; Duration=5;})
+    print("Other Gui Script is Enabled")
+end
+
+
+-- 
 -- First.lua
-if other_gui == true then
-  loadstring(game:HttpGet(other_Script))();
-    print("'other_gui' Script Enabled")
-  game.StarterGui:SetCore("SendNotification", {Title="Magma-mc.net"; Text="Other Gui Script Enabled"; Duration=5;})
-elseif other_gui == false then
-  print("'other_gui' Script Disabled")
-else
-  error("Value 'other_gui' is not set to True Or False")
+-- 
+if other_gui_loaded == true then
+    game.StarterGui:SetCore("SendNotification", {Title="Magma-mc.net"; Text="Other Gui Script is Enabled"; Duration=5;})
+else 
+    if other_gui == true then
+
+        other_gui_loaded = true
+        loadstring(game:HttpGet(other_Script))();
+        print("'other_gui' Script Enabled")
+        game.StarterGui:SetCore("SendNotification", {Title="Magma-mc.net"; Text="Other Gui Script Enabled"; Duration=5;})
+
+    elseif other_gui == false then
+
+    print("'other_gui' Script Disabled")
+
+    else
+        error("Value 'other_gui' is not set to True Or False")
+    end
 end
+
+-- 
 -- Second.lua
-if auto_rob == true then
-  loadstring(game:HttpGet(AutoRob_Script))();
-    print("'auto_rob' Script Enabled")
-  		game.StarterGui:SetCore("SendNotification", {Title="Magma-mc.net"; Text="Auto Rob Script Enabled"; Duration=5;})
-elseif auto_rob == false then
-  print("'auto_rob' Script Disabled")
+-- 
+if auto_rob_loaded == true then
+    game.StarterGui:SetCore("SendNotification", {Title="Magma-mc.net"; Text="Auto Rob Script is Enabled"; Duration=5;})
 else
-  error("Value 'auto_rob' is not set to True Or False")
+    if auto_rob == true then
+        auto_rob_loaded = true
+        loadstring(game:HttpGet(AutoRob_Script))();
+        print("'auto_rob' Script Enabled")
+        game.StarterGui:SetCore("SendNotification", {Title="Magma-mc.net"; Text="Auto Rob Script Enabled"; Duration=5;})
+
+    elseif auto_rob == false then
+        
+        print("'auto_rob' Script Disabled")
+
+    else
+        error("Value 'auto_rob' is not set to True Or False")
+    end
 end
--- End
+--
